0cab641d25274d1e041d32f684b15440209355aa
patryk-madej <59027134+patryk-madej@users.noreply.github.com>
2020-02-10 16:57:23 +0000

    now extracts the text I need

--- scraper.py
+++ scraper.py
@@ -13,40 +13,51 @@ soup = BeautifulSoup(src, 'lxml')
 
 event_divs = soup.find_all("div",{'class': 'event'})
 
+
+events = []
 for event in event_divs:
     #city_span = event.find('span',{'class': 'label status'})
     #if fnmatch.filter(event, '*London*'):
-    print(fnmatch.filter(event, '*London*'))
+    #print(fnmatch.filter(event, '*London*'))
+    events.append(event.getText()) # <======= maybe turn each complex item in the list into text and then fnmatch
 
+#print(events[3].strip()) # <====== need to delete most whitespace for readability. maybe turn \n's into spaces
 
-#print(events_divs[5].getText())
+feed = []
+for e in events:
+    feed.append(" ".join(e.split())) #deletes all whitespaces and places single space between words
+
+london = []
+for l in feed:
+    if not fnmatch.filter(l, '*London*'):
+        feed.remove(l)
+        #london.append(l)
+    elif fnmatch.filter(l, '*London*'):
+        london.append(l)
+    else:
+        continue
+
+print(london)
+
+#print(event_divs[5].getText())
 
 
 
 '''
 ul_column = soup.find_all("ul")[3] #upcoming events
 cities = ul_column.find_all("span")
-
 events = []
 div_tag = ul_column.find_all("div")
-
 for x in cities:
     if re.search("London", x): 
         print(x)
     else:
         continue
-
-
-
-
-
-
 for x in div_tag:
     if div_tag[x]["class"]:
         events.append(div_tag)
     else:
         continue
-
 print(events)
 '''
 
@@ -60,7 +71,6 @@ for div_tag in soup.find_all("div"):
     span_tag = div_tag.find("span")
     events.append(span_tag)
     print(events)
-
 '''
 
 #div_tag = soup.find_all("div")[9]
@@ -76,17 +86,13 @@ for div_tag in soup.find_all("div"):
     if events.index('London'):
         print(div_tag)
     pass
-
-
 '''
 
 
 '''
 events = soup.find_all("div")
-
 for event in events:
     if "event" in event.text:
         print(event)
         #print(link.attrs['label status'])
-
 '''
